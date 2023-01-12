7a10528a3f295f1191fc72454430d22c638f122c
Peter <babyghost@gmail.com>
2023-01-12 17:28:02 -0600

    Initial Commit

---
+++ mvvm_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm_swift
+//
+//  Created by Peter Leung on 12/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
